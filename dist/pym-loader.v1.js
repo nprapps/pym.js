514a73b702e9f0eb2b597f062cd74b9c6ab3e36e
jjelosua <jelosua@npr.org>
2017-07-20 09:57:32 -0400

    tweak example & update date of publication

--- dist/pym-loader.v1.js
+++ dist/pym-loader.v1.js
@@ -1,4 +1,4 @@
-/*! pym-loader.js - v1.3.0 - 2017-07-19 */
+/*! pym-loader.js - v1.3.0 - 2017-07-20 */
 /*
 * pym-loader.js is a wrapper library that deals with particular CMS scenarios to successfully load Pym.js into a given page
 * To find out more about Pym.js check out the docs at http://blog.apps.npr.org/pym.js/ or the readme at README.md for usage.
